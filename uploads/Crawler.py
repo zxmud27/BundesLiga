e24b079d0f7b0a31489bf2b3f649b75d0af84821
Nikita260298 <nikita.tkatschenko@student.uni-tuebingen.de>
2020-05-17 20:34:29 +0200

    Spieldaten fürs ein beliebiges Jahr möglich

--- uploads/Crawler.py
+++ uploads/Crawler.py
@@ -1,58 +1,72 @@
 import json , urllib.request
-def crawler_fun(Jahr, Spieltag):
+def crawler_fun(Jahr):
+
 
     Endergebnis_Spieltag = []
-    # Daten von Openligadb
-    Spieltaglink = ("https://www.openligadb.de/api/getmatchdata/bl1/") + str(Jahr) + ("/") + str(Spieltag)
 
-    # zieht Daten eines Spieltages
-    req = urllib.request.urlopen(Spieltaglink)
-    jso = json.loads(req.read().decode())
+    Endergebnisse_Jahr = []
 
-    # Funktion schaut jede Begegnung eines Spieltages an
+    # Schleife für alle Spieltage
+    for i in range(1,34):
+
+        # Daten von Openligadb
+        Spieltaglink = ("https://www.openligadb.de/api/getmatchdata/bl1/") + str(Jahr) + ("/") + str(i)
+
+        # zieht Daten eines Spieltages
+        req = urllib.request.urlopen(Spieltaglink)
+        jso = json.loads(req.read().decode())
+
+         # Funktion schaut jede Begegnung eines Spieltages an
     
-    for f in jso:
+
+        for f in jso:
         
-        # Namen der spielenden Heimmannschaft , Gastmannschaft
-        Teamlist = []
+            # Namen der spielenden Heimmannschaft , Gastmannschaft
+            Teamlist = []
 
-        # Speichert Heimmannschaft
-        team1 = dict.get(f, "Team1")
-        name1 = dict.get(team1, "TeamName")
-        Heim = str(name1)
-        Teamlist.append(Heim)
+            # Speichert Heimmannschaft
+            team1 = dict.get(f, "Team1")
+            name1 = dict.get(team1, "TeamName")
+            Heim = str(name1)
+            Teamlist.append(Heim)
 
-        # Speichert Gastmannschaft
-        team2 = dict.get(f, "Team2")
-        name2 = dict.get(team2, "TeamName")
-        Gast = str(name2)
-        Teamlist.append(Gast)
+            # Speichert Gastmannschaft
+            team2 = dict.get(f, "Team2")
+            name2 = dict.get(team2, "TeamName")
+            Gast = str(name2)
+            Teamlist.append(Gast)
 
-        Endergebnis_Spieltag.append(Teamlist)
+            Endergebnis_Spieltag.append(Teamlist)
 
-        # Ergebnis Heimmannschaft , Gastmannschaft
-        Goalslist = []
+            # Tore von der Heimmannschaft , Gastmannschaft
+            Goalslist = []
 
-        # Endergebnis
-        MaRe = dict.get(f, "MatchResults")
+            # Endergebnis
+            MaRe = dict.get(f, "MatchResults")
         
-        # Tore Heimmannschaft
-        goalsteam1 = dict.get(MaRe[0], "PointsTeam1")
-        Heimtor = str(goalsteam1)
-        Goalslist.append(Heimtor)
+            # Tore Heimmannschaft
+            goalsteam1 = dict.get(MaRe[0], "PointsTeam1")
+            Heimtor = str(goalsteam1)
+            Goalslist.append(Heimtor)
 
-        # Tore Gastmannschaft
-        goalsteam2 = dict.get(MaRe[0], "PointsTeam2")
-        Gasttor = str(goalsteam2)
-        Goalslist.append(Gasttor)
+            # Tore Gastmannschaft
+            goalsteam2 = dict.get(MaRe[0], "PointsTeam2")
+            Gasttor = str(goalsteam2)
+            Goalslist.append(Gasttor)
 
-        Endergebnis_Spieltag.append(Goalslist)
+            # Zusammenfügen der Spiele am Spieltag
+            Endergebnis_Spieltag.append(Goalslist)
+            # Spieltag einfügen in alle gespielte Spiele
+            Endergebnisse_Jahr.append(Endergebnis_Spieltag)
+            
     
-    print(Endergebnis_Spieltag) 
-    return Endergebnis_Spieltag
+    #print(Endergebnis_Spieltag)
+    #print(Endergebnisse_Jahr) 
+    return Endergebnisse_Jahr
     
    
-#Jahr = input("Gibt mir das Jahr an! ")
-#Tag = input("Vom welchen Spieltag willst du die Daten erhalten? ")
+# Test Jahr
+crawler_fun(2018)
 
-crawler_fun(2018,1)
+#Jahr = input("Von welchem Jahr sollen die Spieldaten geladen werden? ")
+#crawler_fun(Jahr)
