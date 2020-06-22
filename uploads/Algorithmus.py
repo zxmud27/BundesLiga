039a628c00ab598aea7eb851279bc6c3b9a974fb
zxmud27 <Rinor.Kelmendi@student.uni-tuebingen.de>
2020-06-23 01:16:29 +0200

    Oberflächenupdate

--- uploads/Algorithmus.py
+++ uploads/Algorithmus.py
@@ -3,9 +3,9 @@ import os.path
 from os import path
 from Crawler import crawler_fun
 
-def get_all_matches(start_season_day ,start_season_year,end_season_day):
+def get_all_matches(Jahr):
     # CSV Name
-    filename = str(start_season_year) + ('.csv')
+    filename = str(Jahr) + ('.csv')
     teams = []
     
      # Reading the teams
@@ -14,97 +14,88 @@ def get_all_matches(start_season_day ,start_season_year,end_season_day):
         line_count = 0
          # Counter for played games
         k = 0
-        startday_counter = 0
          # CSV file is read and evaluated
         for i in csv_reader:
-            
-            startday_counter += 1
-            if (startday_counter / 9) + 1 > start_season_day and (startday_counter / 9) <= end_season_day :
-                # are all teamnames signed in? is set to false in the first 9 games
+            # are all teamnames signed in? is set to false in the first 9 games
                 # Name of the 2 competing teams
-                names = i[0]
-                # goals ratio after the match
-                goals = i[1]
+            names = i[0]
+            # goals ratio after the match
+            goals = i[1]
 
-                #Auftrennung von "names" -> Teamname beider Teams wird entnommen
-                both_teams = names.strip('["]').split(', ') 
-                teama = both_teams[0]
-                teama = teama[1:-1]
-                teamb = both_teams[1]
-                teamb = teamb[1:-1]
+            #Auftrennung von "names" -> Teamname beider Teams wird entnommen
+            both_teams = names.strip('["]').split(', ') 
+            teama = both_teams[0]
+            teama = teama[1:-1]
+            teamb = both_teams[1]
+            teamb = teamb[1:-1]
 
-                #Seperation of goals, team a's goals: goals_a, team b's goals: goals_b
-                all_goals = goals.strip('["]').split(', ') 
-                goals_a = int(all_goals[0])
-                goals_b = int(all_goals[1])
+            #Seperation of goals, team a's goals: goals_a, team b's goals: goals_b
+            all_goals = goals.strip('["]').split(', ') 
+            goals_a = int(all_goals[0])
+            goals_b = int(all_goals[1])
             
-                # enters names in "teams" for the first 9 matches
-                if k < 9:
+           # enters names in "teams" for the first 9 matches
+            if k < 9:
                 
-                    teamstatsa = []   
-                    teamstatsa.append(teama)
-                    teamstatsa.append([[],[],[]])
-                    teams.append(teamstatsa)
+                teamstatsa = []   
+                teamstatsa.append(teama)
+                teamstatsa.append([[],[],[]])
+                teams.append(teamstatsa)
 
-                    teamstatsb = []
-                    teamstatsb.append(teamb)
-                    teamstatsb.append([[],[],[]])
-                    teams.append(teamstatsb)
-                k += 1
+                teamstatsb = []
+                teamstatsb.append(teamb)
+                teamstatsb.append([[],[],[]])
+                teams.append(teamstatsb)
+            k += 1
             
-                # counter for position of the wanted team
+             # counter for position of the wanted team
+            c = 0
+
+            # teams[get teamstats][in [1] = all stats][0= won  1 = draw, 2 lose]
+            # team[][][]
+            if goals_a > goals_b :
+                for x in teams:
+                    if x[0]== teama:
+                        teams[c][1][0].append(teamb)
+                    c += 1
                 c = 0
-
-                # teams[get teamstats][in [1] = all stats][0= won  1 = draw, 2 lose]
-                # team[][][]
-                if goals_a > goals_b :
-                    for x in teams:
-                        if x[0]== teama:
-                            teams[c][1][0].append(teamb)
-                        c += 1
-                    c = 0
-                    for y in teams:
-                        if y[0] == teamb:
-                            teams[c][1][2].append(teama)
-                        c += 1
+                for y in teams:
+                    if y[0] == teamb:
+                        teams[c][1][2].append(teama)
+                    c += 1
                 
-                elif goals_a < goals_b:
-                    for x in teams:
-                        if x[0] == teamb:
-                            teams[c][1][0].append(teama)
-                        c += 1
-                    c = 0
-                    for y in teams:
-                        if y[0] == teama:
-                            teams[c][1][2].append(teamb)
-                        c += 1
+            elif goals_a < goals_b:
+                for x in teams:
+                    if x[0] == teamb:
+                         teams[c][1][0].append(teama)
+                    c += 1
+                c = 0
+                for y in teams:
+                    if y[0] == teama:
+                        teams[c][1][2].append(teamb)
+                    c += 1
 
-                elif goals_a == goals_b:
-                    for x in teams:
-                        if x[0] == teama:
-                            teams[c][1][1].append(teamb)
-                        c += 1
-                    c = 0
-                    for y in teams:
-                        if y[0] == teamb:
-                            teams[c][1][1].append(teama)
-                        c += 1         
+            elif goals_a == goals_b:
+                for x in teams:
+                    if x[0] == teama:
+                        teams[c][1][1].append(teamb)
+                    c += 1
+                c = 0
+                for y in teams:
+                    if y[0] == teamb:
+                        teams[c][1][1].append(teama)
+                    c += 1         
 
         return teams
             
-def get_win_ratio(home, away, start_day, start_year ,end_day, end_year):
+def get_win_ratio(home, away):
     all_stats = []
-    match_year = start_year 
-    while match_year <= end_year:
-        if not path.exists(str(match_year) + ('.csv')):
-            crawler_fun(match_year) 
-        if match_year == start_year == end_year:
-            all_stats.append(get_all_matches(start_day ,match_year,end_day))
-        elif match_year == start_year:
-            all_stats.append(get_all_matches(start_day,match_year,34))
-        elif match_year == end_year:
-            all_stats.append(get_all_matches(1,match_year,end_day))
-        match_year += 1
+    for i in range(10):
+        file = 2009 + i
+        if not path.exists(str(file) + ('.csv')):
+            crawler_fun(file) 
+        all_stats.append(get_all_matches(file))
+
     win = 0
     draw = 0
     lose = 0
@@ -139,5 +130,4 @@ def get_win_ratio(home, away, start_day, start_year ,end_day, end_year):
     print(ratio)
     return ratio
 
-#get_win_ratio('FC Bayern',"Borussia Dortmund",1,2018,10,2018)
-#get_all_matches(1,2018,3)
+#get_win_ratio('FC Bayern',"Borussia Dortmund")
--- uploads/OberflächeTeamprojekt.py
+++ uploads/OberflächeTeamprojekt.py
@@ -8,7 +8,7 @@ root = tk.Tk()
 root.title("GUI")
 canvas = tk.Canvas(root, height=400, width=400)
 canvas.pack()
-frame = tk.Frame(root, bg='cornflowerblue')
+frame = tk.Frame(root, bg='gray13')
 frame.place(relwidth=1, relheight=1)
 #Algo Objekt, Crawler Objekt
 #algo1 = Algo()
@@ -75,14 +75,14 @@ def Ubersetzer(teamname):
       return 'RB Leipzig'
 def buttonupdate(buttonnummer, teamname):
    if(buttonnummer==1):
-      erstesTeamButton = tk.Button(frame,image= teamname,width=10, height=10, fg="white", bg="royalblue",command=lambda: newframe(1))
+      erstesTeamButton = tk.Button(frame,image= teamname,width=10, height=10, fg="white", bg='gray13',command=lambda: newframe(1))
       erstesTeamButton.place(relx=0.20,rely=0.52, relwidth=0.20, relheight=0.20) 
       global tname1 
       tname1=teamname
       newWindow.destroy()
       
    else:
-      zweitesTeamButton = tk.Button(frame,image= teamname,width=10, height=10, fg="white", bg="royalblue",command=lambda:newframe(2))
+      zweitesTeamButton = tk.Button(frame,image= teamname,width=10, height=10, fg="white", bg='gray13',command=lambda:newframe(2))
       zweitesTeamButton.place(relx=0.60,rely=0.52, relwidth=0.20, relheight=0.20)
       global tname2
       tname2 = teamname
@@ -93,51 +93,51 @@ def newframe(buttonnummer):
    newWindow= tk.Toplevel(root)
    canvas = tk.Canvas(newWindow, height=900, width=400)
    canvas.pack()
-   frame2 = tk.Frame(newWindow, bg='cornflowerblue')
+   frame2 = tk.Frame(newWindow, bg='gray13')
    frame2.place(relwidth=1, relheight=1)
-   BM =  tk.Button(frame2,image=bayernMuenchen,bg="royalblue",command=lambda: buttonupdate(buttonnummer,bayernMuenchen))
+   BM =  tk.Button(frame2,image=bayernMuenchen,bg='gray13',command=lambda: buttonupdate(buttonnummer,bayernMuenchen))
    BM.place(relx=0.0,rely=0.0,relwidth=1/2,relheight=1/9)
-   DM = tk.Button(frame2,image=dortmund,bg="royalblue",command=lambda: buttonupdate(buttonnummer,dortmund))
+   DM = tk.Button(frame2,image=dortmund,bg='gray13',command=lambda: buttonupdate(buttonnummer,dortmund))
    DM.place(relx=0.5,rely=0.0,relwidth=1/2,relheight=1/9)
-   MG = tk.Button(frame2,image=moenchengladbach,bg="royalblue",command=lambda: buttonupdate(buttonnummer,moenchengladbach))
+   MG = tk.Button(frame2,image=moenchengladbach,bg='gray13',command=lambda: buttonupdate(buttonnummer,moenchengladbach))
    MG.place(relx=0.0,rely=1/9,relwidth=1/2,relheight=1/9)
-   RL = tk.Button(frame2,image=rbLeipzig,bg="royalblue",command=lambda: buttonupdate(buttonnummer,rbLeipzig))
+   RL = tk.Button(frame2,image=rbLeipzig,bg='gray13',command=lambda: buttonupdate(buttonnummer,rbLeipzig))
    RL.place(relx=0.5,rely=1/9,relwidth=1/2,relheight=1/9)
-   LK = tk.Button(frame2,image=leverkusen,bg="royalblue",command=lambda: buttonupdate(buttonnummer,leverkusen))
+   LK = tk.Button(frame2,image=leverkusen,bg='gray13',command=lambda: buttonupdate(buttonnummer,leverkusen))
    LK.place(relx=0.0,rely=2/9,relwidth=1/2,relheight=1/9)
-   WB = tk.Button(frame2,image=wolfsburg,bg="royalblue",command=lambda: buttonupdate(buttonnummer,wolfsburg))
+   WB = tk.Button(frame2,image=wolfsburg,bg='gray13',command=lambda: buttonupdate(buttonnummer,wolfsburg))
    WB.place(relx=0.5,rely=2/9,relwidth=1/2,relheight=1/9)
-   FB = tk.Button(frame2,image=freiburg,bg="royalblue",command=lambda: buttonupdate(buttonnummer,freiburg))
+   FB = tk.Button(frame2,image=freiburg,bg='gray13',command=lambda: buttonupdate(buttonnummer,freiburg))
    FB.place(relx=0.0,rely=3/9,relwidth=1/2,relheight=1/9)
-   S4 = tk.Button(frame2,image=schalke04,bg="royalblue",command=lambda: buttonupdate(buttonnummer,schalke04))
+   S4 = tk.Button(frame2,image=schalke04,bg='gray13',command=lambda: buttonupdate(buttonnummer,schalke04))
    S4.place(relx=0.5,rely=3/9,relwidth=1/2,relheight=1/9)
-   HH = tk.Button(frame2,image=hoffenheim,bg="royalblue",command=lambda: buttonupdate(buttonnummer,hoffenheim))
+   HH = tk.Button(frame2,image=hoffenheim,bg='gray13',command=lambda: buttonupdate(buttonnummer,hoffenheim))
    HH.place(relx=0.0,rely=4/9,relwidth=1/2,relheight=1/9)
-   HN =  tk.Button(frame2,image=hannover,bg="royalblue",command=lambda: buttonupdate(buttonnummer,hannover))
+   HN =  tk.Button(frame2,image=hannover,bg='gray13',command=lambda: buttonupdate(buttonnummer,hannover))
    HN.place(relx=0.5,rely=4/9,relwidth=1/2,relheight=1/9)
-   HBSC= tk.Button(frame2,image=herthaBSC,bg="royalblue",command=lambda: buttonupdate(buttonnummer,herthaBSC))
+   HBSC= tk.Button(frame2,image=herthaBSC,bg='gray13',command=lambda: buttonupdate(buttonnummer,herthaBSC))
    HBSC.place(relx=0.0,rely=5/9,relwidth=1/2,relheight=1/9)
-   ST = tk.Button(frame2,image=stuttgart,bg="royalblue",command=lambda: buttonupdate(buttonnummer,stuttgart))
+   ST = tk.Button(frame2,image=stuttgart,bg='gray13',command=lambda: buttonupdate(buttonnummer,stuttgart))
    ST.place(relx=0.5,rely=5/9,relwidth=1/2,relheight=1/9)
-   EF = tk.Button(frame2,image=eintrachtFrankfurt,bg="royalblue",command=lambda: buttonupdate(buttonnummer,eintrachtFrankfurt))
+   EF = tk.Button(frame2,image=eintrachtFrankfurt,bg='gray13',command=lambda: buttonupdate(buttonnummer,eintrachtFrankfurt))
    EF.place(relx=0.0,rely=6/9,relwidth=1/2,relheight=1/9)
-   AB = tk.Button(frame2,image=augsburg,bg="royalblue",command=lambda: buttonupdate(buttonnummer,augsburg))
+   AB = tk.Button(frame2,image=augsburg,bg='gray13',command=lambda: buttonupdate(buttonnummer,augsburg))
    AB.place(relx=0.5,rely=6/9,relwidth=1/2,relheight=1/9)
-   M5 = tk.Button(frame2,image=mainz05,bg="royalblue",command=lambda: buttonupdate(buttonnummer,mainz05))
+   M5 = tk.Button(frame2,image=mainz05,bg='gray13',command=lambda: buttonupdate(buttonnummer,mainz05))
    M5.place(relx=0.0,rely=7/9,relwidth=1/2,relheight=1/9)
-   FD = tk.Button(frame2,image=fortunaduesseldorf,bg="royalblue",command=lambda: buttonupdate(buttonnummer,fortunaduesseldorf))
+   FD = tk.Button(frame2,image=fortunaduesseldorf,bg='gray13',command=lambda: buttonupdate(buttonnummer,fortunaduesseldorf))
    FD.place(relx=0.5,rely=7/9,relwidth=1/2,relheight=1/9)
-   WB = tk.Button(frame2,image=werderBremen,bg="royalblue",command=lambda: buttonupdate(buttonnummer,werderBremen))
+   WB = tk.Button(frame2,image=werderBremen,bg='gray13',command=lambda: buttonupdate(buttonnummer,werderBremen))
    WB.place(relx=0.0,rely=8/9,relwidth=1/2,relheight=1/9)
-   NN = tk.Button(frame2,image=nurnberg,bg="royalblue",command=lambda: buttonupdate(buttonnummer,nurnberg))
+   NN = tk.Button(frame2,image=nurnberg,bg='gray13',command=lambda: buttonupdate(buttonnummer,nurnberg))
    NN.place(relx=0.5,rely=8/9,relwidth=1/2,relheight=1/9)
 
 
-erstesTeamButton = tk.Button(frame,image=dortmund,width=10, height=10, fg="white", bg="royalblue", command=lambda: newframe(1))
+erstesTeamButton = tk.Button(frame,image=dortmund,width=10, height=10, bg='gray13', command=lambda: newframe(1))
 erstesTeamButton.place(relx=0.20,rely=0.52, relwidth=0.20, relheight=0.20)  
 
 
-zweitesTeamButton = tk.Button(frame,image=wolfsburg,width=100, height=10, fg="white", bg="royalblue", command=lambda: newframe(2))
+zweitesTeamButton = tk.Button(frame,image=wolfsburg,width=100, height=10, bg='gray13', command=lambda: newframe(2))
 zweitesTeamButton.place(relx=0.60,rely=0.52, relwidth=0.20, relheight=0.20)
 
 #Checkbox Inhalt ausgeben in Konsole über Button Befehl.
@@ -147,18 +147,18 @@ zweitesTeamButton.place(relx=0.60,rely=0.52, relwidth=0.20, relheight=0.20)
 #IncludeSeasonButtonCheckBox.place(relx=0.35,rely=0.165, relwidth=0.4, relheight=0.03)
 #def buttonclick():
 #     print(buttonstatus.get())
-#erstesTeamButton = tk.Button(frame, text="erstes Team",width=10, height=10, fg="white", bg="royalblue",command=buttonclick)
+#erstesTeamButton = tk.Button(frame, text="erstes Team",width=10, height=10, fg="white", bg='gray13',command=buttonclick)
 #erstesTeamButton.place(relx=0.20,rely=0.52, relwidth=0.2, relheight=0.2)
-CrawlerButton = tk.Button(frame, text="Start Crawler",width=30, height=5, fg="white", bg="royalblue", padx=10, pady=5 )
-CrawlerButton.place(relx=0.06,rely=0.13, relwidth=0.2, relheight=0.1)
+CrawlerButton = tk.Button(frame, text="Start Crawler",width=30, height=5, fg="white", bg='brown4', padx=10, pady=5 )
+CrawlerButton.place(relx=0.06,rely=0.25, relwidth=0.2, relheight=0.1)
 
-IncludeSeasonButtonCheckBox = tk.Checkbutton(frame, text="Include current Season", bg="cornflowerblue")
-IncludeSeasonButtonCheckBox.place(relx=0.35,rely=0.165, relwidth=0.4, relheight=0.03)
+IncludeSeasonButtonCheckBox = tk.Checkbutton(frame, text="Include current Season", bg='gray13',fg="white")
+IncludeSeasonButtonCheckBox.place(relx=0.35,rely=0.285, relwidth=0.4, relheight=0.03)
 tname1=dortmund
 tname2=wolfsburg
-predictButton = tk.Button(frame, text="Predict",width=10, height=10, fg="white", bg="royalblue",command=lambda:winrateLabels(tname1,tname2))
+predictButton = tk.Button(frame, text="Predict",width=10, height=10, fg="white", bg='brown4',command=lambda:winrateLabels(tname1,tname2))
 predictButton.place(relx=0.4,rely=0.75, relwidth=0.2, relheight=0.08)
-startTrain = tk.Button(frame, text="Start Training",width=10, height=10, fg="white", bg="royalblue")
+startTrain = tk.Button(frame, text="Start Training",width=10, height=10, fg="white", bg='brown4')
 startTrain.place(relx=0.4,rely=0.38, relwidth=0.2, relheight=0.08)
 #Methode 
 def winrateLabels(team1, team2):
@@ -166,42 +166,44 @@ def winrateLabels(team1, team2):
 
    #algo1.get_win_ratio(Ubersetzer(team1),Ubersetzer(team2))
    # Winrate
-   pWH = tk.Label(frame, text=stats[0], bg='cornflowerblue') #Übersetzer für team1 und team2
+   pWH = tk.Label(frame, text=stats[0], bg='gray13',fg='white') #Übersetzer für team1 und team2
    pWH.place(relx=0.20,rely=0.9, relwidth=0.2, relheight=0.04)
    # Drawrate
-   pDraw = tk.Label(frame, text=stats[1], bg='cornflowerblue')
+   pDraw = tk.Label(frame, text=stats[1], bg='gray13',fg='white')
    pDraw.place(relx=0.40,rely=0.9, relwidth=0.2, relheight=0.04)
    # Loserate
-   pWA = tk.Label(frame, text=stats[2], bg='cornflowerblue')
+   pWA = tk.Label(frame, text=stats[2], bg='gray13',fg='white')
    pWA.place(relx=0.6,rely=0.9, relwidth=0.2, relheight=0.04)
 
 
 #Labels
-winHome = tk.Label(frame, text="Win Home", bg='cornflowerblue')
+winHome = tk.Label(frame, text="Win Home", bg='gray13',fg='white')
 winHome.place(relx=0.20,rely=0.85, relwidth=0.2, relheight=0.04)
-pWH = tk.Label(frame, text="PercentWH", bg='cornflowerblue') #Übersetzer für team1 und team2
+pWH = tk.Label(frame, text="PercentWH", bg='gray13',fg='white') #Übersetzer für team1 und team2
 pWH.place(relx=0.20,rely=0.9, relwidth=0.2, relheight=0.04)
-draw = tk.Label(frame, text="Draw", bg='cornflowerblue')
+draw = tk.Label(frame, text="Draw",bg='gray13',fg='white')
 draw.place(relx=0.45,rely=0.85, relwidth=0.1, relheight=0.04)
-pDraw = tk.Label(frame, text="PercentDRAW", bg='cornflowerblue')
+pDraw = tk.Label(frame, text="PercentDRAW", bg='gray13',fg='white')
 pDraw.place(relx=0.40,rely=0.9, relwidth=0.2, relheight=0.04)
 
-winAway = tk.Label(frame, text="Win Away", bg='cornflowerblue')
+winAway = tk.Label(frame, text="Win Away", bg='gray13',fg='white')
 winAway.place(relx=0.63,rely=0.85, relwidth=0.14, relheight=0.04)
-pWA = tk.Label(frame, text="PercentWA", bg='cornflowerblue')
+pWA = tk.Label(frame, text="PercentWA", bg='gray13',fg='white')
 pWA.place(relx=0.6,rely=0.9, relwidth=0.2, relheight=0.04)
 
 #currentSeason = tk.Label(frame, text="Include Current Season", bg='cornflowerblue', fg='white')
 #currentSeason.place(relx=0.4,rely=0.165, relwidth=0.3, relheight=0.03)
-punkt1 = tk.Label(frame, text="Oberer Schwarzer Punkt", bg="black", fg="black")
+punkt1 = tk.Label(frame, text="Oberer Schwarzer Punkt", bg='gray13', fg='gray13')
 punkt1.place(relx=0.5,rely=0.60, relwidth=0.02, relheight=0.02)
-punkt2 = tk.Label(frame, text="Unterer Schwarzer Punkt", bg="black", fg="black")
+punkt2 = tk.Label(frame, text="Unterer Schwarzer Punkt", bg='gray13', fg='gray13')
 punkt2.place(relx=0.5,rely=0.63, relwidth=0.02, relheight=0.02)
 
-def show():
-   myLable = Label(root, text=clicked1.get()).pack()
-
-   
+def show():  
+      createLabel()
+  
+def createLabel():
+   myLable = Label(root, text=clicked1.get())
+   myLable.place(relx=0.42,rely=0.95)
 #Lists with all seasons
 seasons_Start = [
    "2009/2010", 
@@ -229,29 +231,28 @@ matchdays_End = matchdays_Start[::-1]
 
 clicked1 = StringVar()  
 clicked1.set(seasons_Start[0])
-season_start_menu = OptionMenu(root, clicked1, *seasons_Start)
-season_start_menu.pack()
-season_start_menu.place(relx=0.05,rely=0.3)
-
+season_start_menu = tk.OptionMenu(root, clicked1, *seasons_Start )
+season_start_menu.place(relx=0.05,rely=0.12)
 clicked2 = StringVar()  
 clicked2.set(seasons_End[0])
 season_end_menu = OptionMenu(root, clicked2, *seasons_End)
-season_end_menu.pack()
-season_end_menu.place(relx=0.55,rely=0.3)
+season_end_menu.place(relx=0.58,rely=0.12)
 
 clicked3 = StringVar()  
 clicked3.set(matchdays_Start[0])
 matchdays_start_menu = OptionMenu(root, clicked3, *matchdays_Start)
-matchdays_start_menu.pack()
-matchdays_start_menu.place(relx=0.35,rely=0.3)
+matchdays_start_menu.place(relx=0.28,rely=0.12)
 
 clicked4 = StringVar()  
 clicked4.set(matchdays_End[0])
 matchdays_end_menu = OptionMenu(root, clicked4, *matchdays_End)
-matchdays_end_menu.pack()
-matchdays_end_menu.place(relx=0.8,rely=0.3)
+matchdays_end_menu.place(relx=0.8,rely=0.12)
 
-selector = tk.Button(root, text="Choose",width=10, height=5, fg="white", bg="royalblue", command=show).pack()
+untilLabel = tk.Label(frame, text="until",bg='gray13',fg='white')
+untilLabel.place(relx=0.46,rely=0.14, relwidth=0.06, relheight=0.03)
+
+selector = tk.Button(root, text="Choose", fg="white", bg='brown4', command=lambda: show())
+selector.place(relx=0.0,rely=0.00, relwidth=1, relheight=0.1) #choose button
 #selector.place(relx=0.4,rely=0.45, relwidth=0.2, relheight=0.08)
 
 
