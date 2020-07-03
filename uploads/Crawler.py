887928059c1fca2c49302110464c817549d4976f
Nikita260298 <nikita.tkatschenko@student.uni-tuebingen.de>
2020-07-03 15:37:31 +0200

    autopeped code

--- uploads/Crawler.py
+++ uploads/Crawler.py
@@ -1,4 +1,5 @@
-import json , urllib.request
+import json
+import urllib.request
 import csv
 
 
@@ -12,11 +13,12 @@ def crawler_fun(Jahr):
     # CSV file name
     filename = str(Jahr) + ('.csv')
 
-     # loop for every season
-    for i in range(1,35):
+    # loop for every season
+    for i in range(1, 35):
 
         # files from Openligadb
-        Spieltaglink = ("https://www.openligadb.de/api/getmatchdata/bl1/") + str(Jahr) + ("/") + str(i)
+        Spieltaglink = (
+            "https://www.openligadb.de/api/getmatchdata/bl1/") + str(Jahr) + ("/") + str(i)
 
        # extracts files of one match day
         req = urllib.request.urlopen(Spieltaglink)
@@ -25,8 +27,8 @@ def crawler_fun(Jahr):
         # function looks for every encounter of one match day
 
         for f in jso:
-            
-             # end result of match
+
+            # end result of match
             MaRe = dict.get(f, "MatchResults")
 
             # verifies if the current match has been played or not
@@ -35,19 +37,19 @@ def crawler_fun(Jahr):
             else:
                 versuch = None
 
-            if versuch != None:
+            if versuch is not None:
                 # name of the playing home team, away team
                 Match = []
 
                # name of the playing home team, away team
                 Teamlist = []
 
-                 # saves home team
+                # saves home team
                 team1 = dict.get(f, "Team1")
                 name1 = dict.get(team1, "TeamName")
                 Teamlist.append(name1)
 
-                 # saves away team
+                # saves away team
                 team2 = dict.get(f, "Team2")
                 name2 = dict.get(team2, "TeamName")
                 Teamlist.append(name2)
@@ -56,9 +58,9 @@ def crawler_fun(Jahr):
 
                 # goals home team, away team
                 Goalslist = []
-        
-                 # goals home team
-            
+
+                # goals home team
+
                 goalsteam1 = dict.get(MaRe[0], "PointsTeam1")
                 Goalslist.append(goalsteam1)
 
@@ -66,21 +68,20 @@ def crawler_fun(Jahr):
                 goalsteam2 = dict.get(MaRe[0], "PointsTeam2")
                 Goalslist.append(goalsteam2)
 
-                 # puts together matches of match day
+                # puts together matches of match day
                 Match.append(Goalslist)
-            
+
                 # date and time of match day
-                get_datum = dict.get(f,"MatchDateTime")
+                get_datum = dict.get(f, "MatchDateTime")
                 Datum = str(get_datum)
                 Match.append(Datum)
 
-                
                 if counter < 9:
-                
+
                     team_icons = []
 
                     team_one_icon = dict.get(team1, "TeamIconUrl")
-                    team_two_icon= dict.get(team2, "TeamIconUrl")
+                    team_two_icon = dict.get(team2, "TeamIconUrl")
 
                     # insert icons into the array
                     team_icons.append(team_one_icon)
@@ -93,17 +94,15 @@ def crawler_fun(Jahr):
                 Endergebnis_Spieltag.append(Match)
 
                 counter += 1
-        # Insert all played matches of the day 
+        # Insert all played matches of the day
         Endergebnisse_Jahr.append(Endergebnis_Spieltag)
         Endergebnis_Spieltag = []
-    
-    
-    with open(filename ,'w', newline='') as f:
+
+    with open(filename, 'w', newline='') as f:
         writer = csv.writer(f)
         for i in Endergebnisse_Jahr:
             for j in i:
                 writer.writerow(j)
 
 #Jahr = input("Von welchem Jahr sollen die Spieldaten geladen werden? ")
-#crawler_fun(2018)
- 
+# crawler_fun(2018)
