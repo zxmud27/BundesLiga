fc47cefbc203735751a37926fcd212cf92a57048
L0kizz <belaumlauf@live.de>
2020-07-07 11:39:46 +0200

    Klasse Team zum herausziehen der Teamlogos in GUI

--- uploads/algorithm_test.py
+++ uploads/algorithm_test.py
@@ -1,5 +1,5 @@
 from Algorithmus import *
-
+import numpy as np
 def test_bayvsBVB():
  win= 10
  tie= 4
@@ -8,6 +8,4 @@ def test_bayvsBVB():
  
  my_result = Algorithmus.get_win_ratio('FC Bayern', "Borussia Dortmund", 1, 2009, 34, 2019)
  result = [win/all, tie/all, lose/all]
- 
- for i in result:
-     assert result[i] == my_result[i]
+ assert np.array_equal(my_result, result)
