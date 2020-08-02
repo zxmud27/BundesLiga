568fbdea80740d196772e577b4a6cbfe3b3e0fe8
L0kizz <belaumlauf@live.de>
2020-08-02 18:41:20 +0200

    GUI objektorientiert und verbessert

---
+++ teamproject/guioopv1.py
@@ -0,0 +1,268 @@
+import tkinter as tk
+import teamproject.crawler
+import teamproject.minimal_algorithm
+import teamproject.poisson_algorithm
+
+class GUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Bundesliga Vorhersage")
+        master.config(bg="black")
+        master.geometry("1200x600")
+
+        
+        #Labels for the next upcoming matchday
+       
+        nextMatchDay = tk.Label(master,text="Next matchday", bg='black',fg='white',font='bold')
+        nextMatchDay.place(relx=0.75,rely=0.18)
+        matchdayTeam1 = tk.Label(master,text="Team 1", bg='black',fg='white', font='bold') 
+        matchdayTeam1.place(relx=0.73,rely=0.25)
+        firstMatchVs = tk.Label(master,text="V.S.", bg='black',fg='white', font='bold') 
+        firstMatchVs.place(relx=0.79,rely=0.25)
+        matchdayTeam2 = tk.Label(master,text="Team 2", bg='black',fg='white', font='bold') 
+        matchdayTeam2.place(relx=0.83,rely=0.25)
+        matchdayTeam3 = tk.Label(master,text="Team 3", bg='black',fg='white', font='bold') 
+        matchdayTeam3.place(relx=0.73,rely=0.3)
+        secondMatchVs = tk.Label(master,text="V.S.", bg='black',fg='white', font='bold') 
+        secondMatchVs.place(relx=0.79,rely=0.3)
+        matchdayTeam4 = tk.Label(master,text="Team 4", bg='black',fg='white', font='bold') 
+        matchdayTeam4.place(relx=0.83,rely=0.3)
+        matchdayTeam5 = tk.Label(master,text="Team 5", bg='black',fg='white', font='bold') 
+        matchdayTeam5.place(relx=0.73,rely=0.35)
+        thirdMatchVs = tk.Label(master,text="V.S.", bg='black',fg='white', font='bold') 
+        thirdMatchVs.place(relx=0.79,rely=0.35)
+        matchdayTeam6 = tk.Label(master,text="Team 6", bg='black',fg='white', font='bold') 
+        matchdayTeam6.place(relx=0.83,rely=0.35)
+        matchdayTeam7 = tk.Label(master,text="Team 7", bg='black',fg='white', font='bold') 
+        matchdayTeam7.place(relx=0.73,rely=0.4)
+        fourthMatchVs = tk.Label(master,text="V.S.", bg='black',fg='white', font='bold') 
+        fourthMatchVs.place(relx=0.79,rely=0.4)
+        matchdayTeam8 = tk.Label(master,text="Team 8", bg='black',fg='white', font='bold') 
+        matchdayTeam8.place(relx=0.83,rely=0.4)
+        matchdayTeam9 = tk.Label(master,text="Team 9", bg='black',fg='white', font='bold') 
+        matchdayTeam9.place(relx=0.73,rely=0.45)
+        fifthMatchVs = tk.Label(master,text="V.S.", bg='black',fg='white', font='bold') 
+        fifthMatchVs.place(relx=0.79,rely=0.45)
+        matchdayTeam10 = tk.Label(master,text="Team 10", bg='black',fg='white', font='bold') 
+        matchdayTeam10.place(relx=0.83,rely=0.45)
+        matchdayTeam11 = tk.Label(master,text="Team 11", bg='black',fg='white', font='bold') 
+        matchdayTeam11.place(relx=0.73,rely=0.5)
+        sixthMatchVs = tk.Label(master,text="V.S.", bg='black',fg='white', font='bold') 
+        sixthMatchVs.place(relx=0.79,rely=0.5)
+        matchdayTeam12 = tk.Label(master,text="Team 12", bg='black',fg='white', font='bold') 
+        matchdayTeam12.place(relx=0.83,rely=0.5)
+        matchdayTeam13 = tk.Label(master,text="Team 13", bg='black',fg='white', font='bold') 
+        matchdayTeam13.place(relx=0.73,rely=0.55)
+        seventhMatchVs = tk.Label(master,text="V.S.", bg='black',fg='white', font='bold') 
+        seventhMatchVs.place(relx=0.79,rely=0.55)
+        matchdayTeam14 = tk.Label(master,text="Team 14", bg='black',fg='white', font='bold') 
+        matchdayTeam14.place(relx=0.83,rely=0.55)
+        matchdayTeam15 = tk.Label(master,text="Team 15", bg='black',fg='white', font='bold') 
+        matchdayTeam15.place(relx=0.73,rely=0.6)
+        eightMatchVs = tk.Label(master,text="V.S.", bg='black',fg='white', font='bold') 
+        eightMatchVs.place(relx=0.79,rely=0.6)
+        matchdayTeam16 = tk.Label(master,text="Team 16", bg='black',fg='white', font='bold') 
+        matchdayTeam16.place(relx=0.83,rely=0.6)
+        matchdayTeam17 = tk.Label(master,text="Team 17", bg='black',fg='white', font='bold') 
+        matchdayTeam17.place(relx=0.73,rely=0.65)
+        ninthMatchVs = tk.Label(master,text="V.S.", bg='black',fg='white', font='bold') 
+        ninthMatchVs.place(relx=0.79,rely=0.65)
+        matchdayTeam18 = tk.Label(master,text="Team 18", bg='black',fg='white', font='bold') 
+        matchdayTeam18.place(relx=0.83,rely=0.65)
+
+        #labes for the win/lose/draw percentages
+        winHome = tk.Label(master, text="Win Home:", bg='black', fg='white',height=3,width=8)
+        winHome.place(relx=0.1, rely=0.6)
+        pWH = tk.Label(master, text="PercentWH", bg='black', fg='white',height=3,width=8) 
+        pWH.place(relx=0.1, rely=0.65)
+        draw = tk.Label(master, text="Draw:", bg='black', fg='white',height=3,width=8)
+        draw.place(relx=0.25, rely=0.6)
+        pDraw = tk.Label(master, text="PercDRAW", bg='black', fg='white',height=3,width=8)
+        pDraw.place(relx=0.25, rely=0.65)
+        winAway = tk.Label(master, text="Win Away:", bg='black', fg='white',height=3,width=8)
+        winAway.place(relx=0.4, rely=0.6)
+        pWA = tk.Label(master, text="PercentWA", bg='black', fg='white',height=3,width=8)
+        pWA.place(relx=0.4, rely=0.65)
+
+        #Label for the season input field 
+        self.searched = tk.StringVar()
+        selectionofseasonText = tk.Label(master,text="Season selection", bg='black', fg='white' )
+        selectionofseasonText.place(relx=0.8,rely=0.01,relwidth=0.2,relheight=0.015)
+        selectionofseason= tk.Entry(master, textvariable=self.searched)
+        selectionofseason.place(relx=0.87,rely=0.03,relwidth=0.061)
+
+        #vs Label between 2 teams
+        vsLabel = tk.Label(
+            master,
+            text="V.S.",
+            bg='black',
+            fg='white', width=3, height=1, font='bold')
+        vsLabel.place(relx=0.26, rely=0.27)
+        untilLabel = tk.Label(master, text="until", bg='black', fg='white',height=2,width=3, font='bold')
+        untilLabel.place(relx=0.33, rely=0.009)
+
+        """dropdown lists for selecting seasons and matchdays for the crawler
+
+        """
+
+        #list with all seasons
+        seasons_Start = [
+            2011,
+            2012,
+            2013,
+            2014,
+            2015,
+            2016,
+            2017,
+            2018,
+            2019
+        ]
+        #reversed list with all seasons
+        seasons_End = seasons_Start[::1]
+
+        # List with all match days
+        matchdays_Start = []
+        for i in range(0, 34):
+            matchdays_Start.append(i+1)
+
+        # reversed list with all match days
+        matchdays_End = matchdays_Start[::-1]
+
+        self.clicked_day_Start = tk.StringVar()
+        self.clicked_day_Start.set(matchdays_Start[0])
+        matchdays_start_menu = tk.OptionMenu(master, self.clicked_day_Start, *matchdays_Start,)
+        matchdays_start_menu.place(relx=0.2, rely=0.015)
+
+        self.clicked_season_Start = tk.StringVar()
+        self.clicked_season_Start.set(seasons_Start[0])
+        season_start_menu = tk.OptionMenu(master, self.clicked_season_Start, *seasons_Start)
+        season_start_menu.place(relx=0.25, rely=0.015)
+
+        self.clicked_day_End = tk.StringVar()
+        self.clicked_day_End.set(matchdays_End[0])
+        matchdays_end_menu = tk.OptionMenu(root, self.clicked_day_End, *matchdays_End)
+        matchdays_end_menu.place(relx=0.38, rely=0.015)
+
+        self.clicked_season_End = tk.StringVar()
+        self.clicked_season_End.set(seasons_End[-1])
+        season_end_menu = tk.OptionMenu(root, self.clicked_season_End,*seasons_End)
+        season_end_menu.place(relx=0.43, rely=0.015)
+
+
+
+        """
+        dropdown list for selecting the league for the crawler
+        """
+        #list with all leagues
+        liga_dropdown = [
+        "1. Bundesliga",
+        "2. Bundesliga",
+        "3. Bundesliga",
+        "1. Handball Bundesliga"
+                                                          ]
+
+        self.click_liga_dropdown = tk.StringVar()
+        self.click_liga_dropdown.set(liga_dropdown[0])
+        liga_menu = tk.OptionMenu(master, self.click_liga_dropdown, *liga_dropdown)
+        liga_menu.place(relx=0.6, rely=0.015)
+
+        #Lists which get filled with teams by the crawler
+        
+        
+
+        #dropdown lists for selecting the teams
+        
+
+        #list with all algorithms
+        algo_dropdown=[
+        "Minimal Algorithm",
+        "Poisson Algorithm"
+        ]
+
+        #dropdown list for selecting an algorithm
+        self.click_algo_dropdown = tk.StringVar()
+        self.click_algo_dropdown.set("Choose an algorithm")
+        algo_menu = tk.OptionMenu(master, self.click_algo_dropdown, *algo_dropdown)
+        algo_menu.place(relx=0.21, rely=0.4)   
+
+        #dropdown list for choosing the algorithm
+        #click_registryPatterninput_dropdown = tk.StringVar()
+        #click_registryPatterninput_dropdown.set("Choose an algorithm")
+        #algo_menu = tk.OptionMenu(master, click_registryPatterninput_dropdown, *registryPatterninput)
+        #algo_menu.place(relx=0.01, rely=0.3)
+
+        #button for starting the crawler
+        CrawlerButton = tk.Button(
+            master,
+            text="Start Crawler",
+            width=25,
+            height=2,
+            fg="white",
+            bg='brown4',
+            command = lambda: self.crawler_load())
+        CrawlerButton.place(relx=0.20, rely=0.15)
+
+        #Button for starting the prediction
+        startButton = tk.Button(
+            master,
+            text="Start prediction",
+            width=25,
+            height=2,
+            fg="white",
+            bg='brown4',
+            command = lambda: self.algorithm_load())
+        startButton.place(relx=0.202, rely=0.5)
+
+    def algorithm_load(self):
+        if self.click_algo_dropdown == "Minimaler Algorithmus":
+            algo_m = teamproject.minimal_algorithm.minimal_class()
+            algo_m.get_minimal_probabilities(self.click_teams1_dropdown.get(),self.click_teams2_dropdown.get())
+        elif self.click_algo_dropdown == "Poisson Algorithmus":
+            algo_p = teamproject.poisson_algorithm.poisson_class()
+            algo_p.get_probabilities(self.click_teams1_dropdown.get(),self.click_teams2_dropdown.get())
+
+
+    def crawler_load(self):
+        teams1_list = []
+        teams2_list = []
+        teams1_list = teamproject.crawler.DataCrawler().getNamelist(int(self.searched.get()),self.click_liga_dropdown.get())
+        teams2_list = teamproject.crawler.DataCrawler().getNamelist(int(self.searched.get()),self.click_liga_dropdown.get())
+        self.click_teams1_dropdown = tk.StringVar()
+        self.click_teams1_dropdown.set(teams1_list[0])
+        team1_menu = tk.OptionMenu(self.master, self.click_teams1_dropdown, *teams1_list)
+        team1_menu.place(relx=0.12, rely=0.265)
+        self.click_teams2_dropdown = tk.StringVar()
+        self.click_teams2_dropdown.set(teams2_list[1])
+
+        team2_menu = tk.OptionMenu(self.master, self.click_teams2_dropdown, *teams2_list)
+        team2_menu.place(relx=0.315, rely=0.265)
+        crawler_class = teamproject.crawler.DataCrawler()
+        crawler_class.getSeasons(int(self.clicked_day_Start.get()),
+                                                int(self.clicked_season_Start.get()), 
+                                                int(self.clicked_day_End.get()), 
+                                                int(self.clicked_season_End.get()), 
+                                                self.click_liga_dropdown.get())
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+        
+root = tk.Tk()
+GUI(root)
+root.mainloop()        
+
+
+
+
